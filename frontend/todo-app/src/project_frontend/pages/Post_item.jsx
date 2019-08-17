a47a31a44849c9739dc1036ff819311df7e936f4
Jeremy Quintana <s3719476@student.rmit.edu.au>
2019-08-18 05:48:27 +1000

    got front end post to back end working

--- frontend/todo-app/src/project_frontend/pages/Post_item.jsx
+++ frontend/todo-app/src/project_frontend/pages/Post_item.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import toBackend from '../toBackend/toBackend.js'
 
 class Post_item extends Component {
   constructor(props){
@@ -11,16 +12,17 @@ class Post_item extends Component {
     }
 
     this.handleChange = this.handleChange.bind(this)
+    this.submitPost = this.submitPost.bind(this)
   }
 
   render() {
     return (
       <div className="Post_item">
-        <form>
+        <form onSubmit={this.submitPost}>
           <label>Item Description: <textarea name="item_description" placeholder="Item Description" value={this.state.item_description} onChange={this.handleChange} /></label><br />
           <label>Item Name: <input required type="text" name="item_name" placeholder="Item Name" value={this.state.item_name} onChange={this.handleChange} /></label><br />
           <label>Item Cost: $<input required type="number" name="item_cost" placeholder="Item Cost" min="0.01" step="0.01" pattern="\d.\d" value={this.state.item_cost} onChange={this.handleChange} /></label><br />
-          <label>Item Image: <input type="file" name="item_img" accept="image/*" /></label><br />
+
           <input type="submit" value="Submit" />
         </form>
       </div>
@@ -33,6 +35,11 @@ class Post_item extends Component {
         [event.target.name] : event.target.value
       }
     )
+  }
+
+  submitPost(event){
+    toBackend.postItem(this.state.item_description, this.state.item_name, this.state.item_cost);
+    event.preventDefault();
   }
 }
 
