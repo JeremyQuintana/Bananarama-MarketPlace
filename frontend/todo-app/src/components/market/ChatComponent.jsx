031ecde0745353dfe2a84e97803b654c9a9e3c74
Mateo <s3708081@student.rmit.edu.au>
2019-08-25 00:33:25 +1000

    on submit adds messages into array

--- frontend/todo-app/src/components/market/ChatComponent.jsx
+++ frontend/todo-app/src/components/market/ChatComponent.jsx
@@ -7,13 +7,12 @@ import "./Chat.css";
 
 const Sending_Data = [
   {
-    senderId: "Buyer",
-    text: "I want to buy"
+    senderId: "Seller",
+    text: "if you want cuzzy"
   },
-
   {
-    senderId: "Buyer",
-    text: "Some Items For uni"
+    senderId: "Seller",
+    text: "What did you want to buy ?"
   }
 ];
 const Receiving_Data = [
@@ -29,28 +28,44 @@ const Receiving_Data = [
 
 const Chats = [
   {
-    Chat: "Reuben",
+    Chat: "Reuben"
   },
-    {
-    Chat: "Val",
+  {
+    Chat: "Val"
   },
-    {
-    Chat: "Mitch",
+  {
+    Chat: "Mitch"
   }
 ];
+
+function addInputText() {
+  var input = document.getElementById("inputtext");
+  // Seller will need to be some sort of id to determine whether they are a buyer or seller
+  Sending_Data.push({ id: "Seller" });
+  Sending_Data.push({ text: input.value });
+  input.value = "";
+  console.log(Sending_Data);
+}
 
 class ChatComponent extends Component {
   render() {
     let retVal = (
       <div>
-        <div className="ChatHead"></div>
-
-        <div className="Body">
-          <CuurentChats></CuurentChats>
-          <Chatwindow></Chatwindow>
-          <InputBox></InputBox>
-         <div className="title"></div>
+        <div className="grid-container">
+          <div className="grid-item">
+            <CuurentChats></CuurentChats>
+          </div>
+          <div className="grid-item">
+            {" "}
+            <div className="Body" id="refresh">
+              <Chatwindow />
+              <InputBox />
+              <div className="title"></div>
+            </div>
+          </div>
         </div>
+
+        <FooterComponent></FooterComponent>
       </div>
     );
     return retVal;
@@ -58,21 +73,79 @@ class ChatComponent extends Component {
 }
 
 function CuurentChats() {
-  return <div className="rooms-list"> {Chats.map((message, index) => {
+  return (
+    <div className="rooms-list">
+      {" "}
+      {Chats.map((message, index) => {
         return (
           <div key={index} className="chats">
             <div className="chat_text">{message.Chat}</div>
           </div>
         );
-      })}</div>;
+      })}
+    </div>
+  );
 }
 
-function InputBox() {
-  return (
-    <form className="InputBox">
-      <input placeholder="SendMessage" type="text" />
-    </form>
-  );
+class InputBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.message);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.sendMessage(this.state.message);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="InputBox">
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          type="text"
+          id="inputtext"
+        />
+
+        <button type="button" id="add" onClick={addInputText}>
+          Send{" "}
+        </button>
+      </form>
+    );
+  }
+}
+
+class Chatwindow extends React.Component {
+  render() {
+    return (
+      <div className="message-list">
+        {Sending_Data.map((message, index) => {
+          return (
+            <div key={index} className="message">
+              <div className="message-username">{message.senderId}</div>
+              <div className="message-text">{message.text}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 function Message() {
@@ -83,31 +156,5 @@ function Message() {
     </div>
   );
 }
-
-function Chatwindow() {
-  return (
-    <div className="message-list">
-      {Sending_Data.map((message, index) => {
-        return (
-          <div key={index} className="message">
-            <div className="message-username">{message.senderId}</div>
-            <div className="message-text">{message.text}</div>
-          </div>
-        );
-      })}
-
-      {Receiving_Data.map((message, index) => {
-        return (
-          <div key={index} className="message_receiver">
-            <div className="message-username">{message.senderId}</div>
-            <div className="receiver_message-text">{message.text}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-
-
 
 export default withRouter(ChatComponent);
