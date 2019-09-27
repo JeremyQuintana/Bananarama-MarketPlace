489edd9291a39cac84da8ddaa4328ce312310e9b
s370718 <s3707187@student.rmit.edu.au>
2019-09-27 22:30:27 +1000

    old work added

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,10 +1,12 @@
 package com.sept.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RestController
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
