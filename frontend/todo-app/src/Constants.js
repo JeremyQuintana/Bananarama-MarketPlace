b389d825753ac3bd83dd13d559ba4b576cf4644e
s370718 <s3707187@student.rmit.edu.au>
2019-09-29 00:57:06 +1000

    fixed, 2 services and IPs

--- frontend/todo-app/src/Constants.js
+++ frontend/todo-app/src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://35.244.106.171:8080'
-export const JPA_API_URL = 'http://35.244.106.171:8080/jpa'
+export const API_URL = 'http://35.197.177.136:8080'
+export const JPA_API_URL = 'http://35.197.177.136:8080/jpa'
