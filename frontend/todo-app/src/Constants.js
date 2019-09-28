a6f47dddca0b324294fd2c0a3fa484d5ef68dd59
s370718 <s3707187@student.rmit.edu.au>
2019-09-28 21:34:16 +1000

    splitting deployment

--- frontend/todo-app/src/Constants.js
+++ frontend/todo-app/src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://35.189.61.182:8080'
-export const JPA_API_URL = 'http://35.189.61.182:8080/jpa'
+export const API_URL = 'http://35.244.106.171:8080'
+export const JPA_API_URL = 'http://35.244.106.171:8080/jpa'
