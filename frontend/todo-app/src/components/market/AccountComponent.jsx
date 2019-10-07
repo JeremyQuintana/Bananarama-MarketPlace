e80a4e55fc1ee3d5ceeb26da10ea2620e6610d3e
s3707187 <s3707187@student.rmit.edu.au>
2019-10-07 20:04:25 +1100

    refactored items component, adding account sections

--- frontend/todo-app/src/components/market/AccountComponent.jsx
+++ frontend/todo-app/src/components/market/AccountComponent.jsx
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import ItemsComponent from "./ItemsComponent"
+
 
 
 class AccountComponent extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            pastPosts: [],
+            currentPosts: [],
+            deletedPosts: [],
+            allChats: [],
+          }
     }
 
     render() {
         let retVal = (
-            <div className="page-header">
-                <h1>Account</h1>
+            <div className="container">
+                <h1 className="marketTitle">Account</h1>
+                <div className="container">
+                    <h1 className="marketTitle">Your Past Sales</h1>
+                    <div className="container">
+                        <ItemsComponent history={this.props.history} backPostings={this.state.pastPosts}></ItemsComponent>
+                    </div>
+                </div>
+                <div className="container">
+                    <h1 className="marketTitle">Your Current Sales</h1>
+                    <div className="container">
+                        <ItemsComponent history={this.props.history} backPostings={this.state.currentPosts}></ItemsComponent>
+                    </div>
+                </div>
+                <div className="container">
+                    <h1 className="marketTitle">Your Deleted Sales</h1>
+                    <div className="container">
+                        <ItemsComponent history={this.props.history} backPostings={this.state.deletedPosts}></ItemsComponent>
+                    </div>
+                </div>
+                {/*<div className="container">
+                    <h1 className="marketTitle">Your Chats</h1>
+                    <div className="container">
+                        <ChatsComponent history={this.props.history} backPostings={this.state.allChats}></ChatsComponent>
+                    </div>
+                </div>*/}
             </div>
         );
         return retVal;
--- frontend/todo-app/src/components/market/MarketComponent.jsx
+++ frontend/todo-app/src/components/market/MarketComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 
 import { withRouter } from "react-router-dom";
 import MarketDataService from "../../api/market/MarketDataService.js"
-
+import ItemsComponent from "./ItemsComponent"
 import './Market.css';
 import toBackend from '../../project_frontend/toBackend/postBackend.js'
 
@@ -82,7 +82,7 @@ render() {
 
               <h1 className="marketTitle">Browse Marketplace</h1>
               <div className="container">
-                  <Items history={this.props.history} backPostings={this.state.backPostings}></Items>
+                  <ItemsComponent history={this.props.history} backPostings={this.state.backPostings}></ItemsComponent>
               </div>
           </div>
       );
@@ -132,54 +132,7 @@ render() {
 
 }
 
-// Helper class to render  the post rows
-class Items extends Component {
-//solution to making itmes component rerender on state change from parent Component
-//found at https://medium.com/p/387720c3cff8/responses/show
-  static getDerivedStateFromProps(props, state){
-    if (props.backPostings !== state.backPostings) {
-      return {backPostings: props.backPostings};
-    }
-    return null;
-  }
 
-  constructor(props) {
-      super(props);
-      // State stores the posts from backend
-      this.state = {
-          backPostings: this.props.backPostings,
-      }
-  }
-
-  render() {
-      var retVal = [];
-      // loop through the postings from backend
-      for (var r = 0; r < this.state.backPostings.length; r++) {
-          let postId = this.state.backPostings[r].id;
-          // Append the row of post information
-          retVal.push(
-              <div className="posting container" onClick={() => this.routeChange(postId)}>
-                  <span className="postTitle"><img src={'post_images/' + this.state.backPostings[r].photo + '.jpg'}></img>{this.state.backPostings[r].title}</span> <br></br>
-                  <span className="postDescription">{this.state.backPostings[r].description}</span> <br></br>
-                  <span className="postPrice">${this.state.backPostings[r].price}</span> <br></br>
-                  <span className="postSeller">{this.state.backPostings[r].ownerId}</span> <br></br>
-
-                  <br></br>
-              </div>
-
-        );
-      }
-
-
-      return retVal;
-  }
-
-  // Method for when a user clicks on a post, route them to post page
-  routeChange(x) {
-      this.props.history.push("/market/" + x);
-  }
-
-}
 
 
 
