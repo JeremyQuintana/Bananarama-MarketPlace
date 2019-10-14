b69fec19a3b5fe86317db0a28bf503f43fa9b132
s3707187 <s3707187@student.rmit.edu.au>
2019-10-14 17:03:34 +1100

    chat updating

--- frontend/todo-app/src/components/market/ChatComponent.jsx
+++ frontend/todo-app/src/components/market/ChatComponent.jsx
@@ -54,7 +54,9 @@ class ChatComponent extends Component {
 
     // needs to be some username at some poiunt
     //var username = "thisUsername";
-    var username = sessionStorage.getItem('authenticatedUser');
+    // var username = sessionStorage.getItem('authenticatedUser');
+    var username = "user1";
+
     const message = { text: itemText, username };
     this.setState({
       message,
@@ -70,8 +72,8 @@ class ChatComponent extends Component {
       console.log(newMessageToOutPut);
 
       // calling it here 
-     // ChatService.addChat("user1","user2",newMessageToOutPut.text);
-     ChatService.addChat(sessionStorage.getItem('authenticatedUser'), "user2",newMessageToOutPut.text);
+      // ChatService.addChat("user1","user2",newMessageToOutPut.text);
+      ChatService.addChat("user1", "user2", newMessageToOutPut.text);
       this.setState({
         messages: messages,
         message: { text: "", key: "" }
@@ -80,197 +82,151 @@ class ChatComponent extends Component {
   };
   render() {
     return (
-     
 
-        <div className="grid-container">
-          <div className="grid-item">
-                     <h1>Chats     </h1>
 
-            <CuurentChats></CuurentChats>
-          </div>
-          <div className="grid-item">
-            <div className="chat">
-              <div className="message-list">
+      <div className="grid-container">
+        <div className="grid-item">
+          <h1>Chats     </h1>
 
-                <MessageObjects
-                  allMeassages={this.state.messages}
-                  deleteItem={this.deleteItem}
-                />
-              </div>
-
-              <MessageList
-                addMessage={this.addMessage}
-                inputElement={this.inputElement}
-                handleInput={this.handleInput}
-                message={this.state.message}
-              
-              />
-              <Returnchats> </Returnchats>
-            </div>
-            
-          </div>
-
-          
+          <CuurentChats></CuurentChats>
         </div>
-      
+        <div className="grid-item">
+          <div className="chat">
+            <div className="message-list">
+
+              <MessageObjects
+                allMeassages={this.state.messages}
+                deleteItem={this.deleteItem}
+              />
+            </div>
+
+            <MessageList
+              addMessage={this.addMessage}
+              inputElement={this.inputElement}
+              handleInput={this.handleInput}
+              message={this.state.message}
+
+            />
+            {/* <TestChats> </TestChats> */}
+          </div>
+
+        </div>
+
+
+      </div>
+
     );
   }
 }
 
+
 class MessageList extends Component {
   render() {
     return (
-        <form onSubmit={this.props.addMessage} className="InputBox">
-          <input
-            placeholder="Enter Message"
-            ref={this.props.inputElement}
-            value={this.props.message.text}
-            onChange={this.props.handleInput}
-          />
-          <button type="submit">
-            {" "}
-            <img alt="bananaLogo" src={require("./banana_icon.png")} />{" "}
-          </button>
-        </form>
+      <form onSubmit={this.props.addMessage} className="InputBox">
+        <input
+          placeholder="Enter Message"
+          ref={this.props.inputElement}
+          value={this.props.message.text}
+          onChange={this.props.handleInput}
+        />
+        <button type="submit">
+          {" "}
+          <img alt="bananaLogo" src={require("./banana_icon.png")} />{" "}
+        </button>
+      </form>
     );
   }
 }
 
 class MessageObjects extends Component {
-   static contextTypes = {
+  static contextTypes = {
     router: PropTypes.object
   }
 
-   constructor(props, context) {
-     super(props, context);
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      newmessages: [],
+
+    }
   }
   create_new_message = message => {
     return (
       <div className="message">
-        <div className="username_id">{sessionStorage.getItem('authenticatedUser')}</div>
+        <div className="username_id">{"user1"}</div>
         <div className="message-text" key={message.key}>
           {message.text}
         </div>
       </div>
     );
   };
+
+  componentDidMount() {
+    this.fetchMessages()
+    this.timer = setInterval(() => this.fetchMessages(), 500000);
+  }
+
+  componentWillUnmount() {
+    this.timer = null;
+  }
+
+
+  fetchMessages() {
+    console.log("poll")
+    ChatService.loadAllChats("user1", "user2").then(
+      response => {
+        this.setState({ newmessages: response.data })
+
+      }
+    ).catch(error => console.log("refreshing chat failed", error));
+  }
+
+
   render() {
     const messages = this.props.allMeassages;
     const listMessages = messages.map(this.create_new_message);
 
-    this.refreshChatLog();
+  //  this.fetchMessages();
 
 
-    return <ul className="messages">{listMessages}</ul>;
-  }
+    //return <ul className="messages">{listMessages}</ul>;
 
-  refreshChatLog() {
-    var user1= "user1";
-    var user2= "user2";
-    window.setInterval(function(){
-      // change this vvv
-      ChatService.loadAllChats("user1", "user2").then(
-        response => {
-          this.setState({ newmessages: response.data }) 
-        
-        }
-      ).catch(error=>console.log("RUNNING REFRESH CAHT", error));
-      // to look like
-      // ChatService.loadChatsAfter(lastChat);
-      // this "lastChat" object is the one sent to you from backend
-      // eg: when you call loadAllChats (you get a list of chats)
+    var retVal = [];
+    for (var r = 0; r < this.state.newmessages.length; r++) {
+      retVal.push(
+        // <div className="message">
+        //   <span className="messages username_id">{this.state.newmessages[r].sender}</span><br></br>
 
-    }, 100000);
-  }
-}
+        //   <span className="messages message-sender">{this.state.newmessages[r].text}</span><br></br>
+        // </div>
+        this.create_new_message(this.state.newmessages[r])
 
-
-
-
-
-
-
-
-
-
-class Returnchats extends Component {
-
-  
-  constructor(props) {
-    super(props);
-
-    // State stores the posts from backend
-    this.state = {
-      newmessages: [],
-        
+      );
     }
-  
-    
-
-  this.refreshChatLog()
-  
-
-   }
-
- //random
-
-render() {
-var retVal = [];
-
-
-for (var r = 0; r < this.state.newmessages.length; r++) {
-
-  retVal.push(
-    <div className="message">
-     <span className="username_id">{this.state.newmessages[r].sender}</span><br></br>
-     
-     <span className="message-sender">{this.state.newmessages[r].text}</span><br></br>
-      </div>
-
-);
-}
 
     return retVal;
+
+
+  }
+
+  
 }
 
 
- 
-
-  refreshChatLog() {
-    
-   // window.setInterval(function(){
-      // change this vvv
-    //
-    //this is for the current logged in user
-    //var user1= sessionStorage.getItem('authenticatedUser');
-
-    var user1= "user1";
-    var user2= "user2";
-      ChatService.loadAllChats(user1, user2).then(
-        response => {
-          this.setState({ newmessages: response.data }) 
-        
-        }
-      ).catch(error=>console.log("RUNNING REFRESH CAHT", error));
-
-          
-        //  console.log(response.data);
-        }
-         
-      
-    
-     
-      
-      // to look like
-      // ChatService.loadChatsAfter(lastChat);
-      // this "lastChat" object is the one sent to you from backend
-      // eg: when you call loadAllChats (you get a list of chats)
-
-//   }, 100000);
-  }
 
 
-  export default withRouter(ChatComponent);
+
+
+
+
+
+
+
+
+
+export default withRouter(ChatComponent);
 
 
 
